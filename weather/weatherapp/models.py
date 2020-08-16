e54cbed3a108a2cddc61bdfe5665bc9f6fc55610
Ayush Tyagi <57788504+ayushtyagispace@users.noreply.github.com>
2020-08-16 13:53:57 +0530

    Update models.py

--- weather/weatherapp/models.py
+++ weather/weatherapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25,unique=True)
 
     def __str__(self):
         return self.name
